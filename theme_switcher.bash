2a599762385a89e6ebe920ee5e20a9ba104affbc
olivier <olibitter@gmail.com>
2022-10-16 19:38:47 +0200

    Simplify nvim theme switching by using default pipe

--- theme_switcher.bash
+++ theme_switcher.bash
@@ -1,12 +1,11 @@
 #!/bin/bash
 # shellcheck disable=2086,2011,2128
-PROJECT_ROOT=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
-source $PROJECT_ROOT/config
-
 THEME=$1
+
 THEME_FILENAME=".theme"
 THEME_HOME="$HOME/.config/theme"
 CURRENT_THEME=$THEME_HOME/$THEME_FILENAME
+XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-"$HOME/.config"}
 
 # Update current theme state
 echo $THEME > $CURRENT_THEME
@@ -17,18 +16,13 @@ tmux source-file "$XDG_CONFIG_HOME/tmux/themes/$THEME.tmux"
 # Update kitty theme
 kitty +kitten themes --reload-in=all --config-file-name=theme-loader.conf $THEME
 
-# Update nvim theme in all listening nvim servers
-[[ -d $NVIM_PIPES ]] || mkdir $NVIM_PIPES
-ls $NVIM_PIPES | xargs -I {} sh -c "nvim --server $NVIM_PIPES/{} --remote-send ':colorscheme $THEME-mod<CR>'"
-
-XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-"$HOME/.config"}
+ls $XDG_RUNTIME_DIR/nvim.*.0 | xargs -I {} nvim --server {} --remote-send "<cmd>colorscheme $THEME-mod<cr>"
 
 configs=(awesome kitty tmux zsh nvim)
 
 for config in "${configs[@]}"; do
     echo $THEME > $XDG_CONFIG_HOME/$config/.theme
 done
-
 
 # Restart awesome
 # kill -s SIGHUP $(pgrep awesome)
